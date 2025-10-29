42bdbcb6f9f11c34474a0cd0393464f16b6ddda3
aden ali <65180119+adenali20@users.noreply.github.com>
2025-10-28 22:49:55 -0500

    Update success message in get method

--- src/main/java/com/xp/xpposts/controller/UserController.java
+++ src/main/java/com/xp/xpposts/controller/UserController.java
@@ -30,7 +30,7 @@ public class UserController {
     }
     @GetMapping("/user/get")
     public String get(){
-      return "successfull from deply";
+      return "successfull from deply-1";
     }
 
 }
